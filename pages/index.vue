e40e03567e081ef16229f8a9a782242f9553c042
Sebastian Lopez Buritica <selobu@gmail.com>
2022-09-07 09:55:51 -0500

    adding some data

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">GextionHSEQ</h1>
+      <h2 class="subtitle text-center">CMS</h2>
     </section>
 
     <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Último post</h3>
       <posts post-type="blog" :amount="1" />
     </section>
   </main>
