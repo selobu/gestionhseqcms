81b9e9140168a415b3a07ff64ed243bd00697ba7
Sebastian Lopez Buritica <selobu@gmail.com>
2022-09-30 15:34:26 -0500

    updating index to 20 posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,7 @@
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
       <h1 class="title">Blog</h1>
-      <posts post-type="blog" :amount="10" />
+      <posts post-type="blog" :amount="20" />
     </section>
   </main>
 </template>
